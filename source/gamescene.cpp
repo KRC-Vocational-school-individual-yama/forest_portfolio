84e435c5aefdffdab99ba25612bd81463ea3f981
kusamotti <1y2m3m4t5y6u7i8t@gmail.com>
2023-04-28 23:21:45 +0900

    remove bug
    debugPlankを消した

--- source/gamescene.cpp
+++ source/gamescene.cpp
@@ -2,7 +2,7 @@
 
 #include<DxLib.h>
 #include"libs/scenemanager.h"
-#include"debug/debug_Prank.h"
+//#include"debug/debug_Prank.h"
 
 #include"backImage.h"
 #include"player.h"
@@ -20,7 +20,7 @@ GameScene::GameScene()
 	Create<TalkObjectManager>();
 	Create<Player>();
 	Create<ClickHint>();
-	Create<Debug_Prank>();
+	//Create<Debug_Prank>();
 	Create<GameMasterAi>();
 
 
